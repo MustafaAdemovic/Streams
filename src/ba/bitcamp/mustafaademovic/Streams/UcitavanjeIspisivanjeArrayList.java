258cd3fc39b0cea95f8f53b185e0b3b76f8ce4cb
Mustafa Ademovic <mustafa.ademovic93@gmail.com>
2015-01-22 17:27:41 +0100

    Streams

---
+++ src/ba/bitcamp/mustafaademovic/Streams/UcitavanjeIspisivanjeArrayList.java
@@ -0,0 +1,54 @@
+package ba.bitcamp.mustafaademovic.Streams;
+
+/**
+ * Ucitavanje i ispisivanje pomocu streama array list
+ */
+
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class UcitavanjeIspisivanjeArrayList {
+	public static void main(String[] args) throws IOException {
+		byte[] buffer = new byte[10];
+		InputStream is = new DataInputStream(System.in);
+		OutputStream os = new DataOutputStream(System.out);
+		ArrayList<Byte> bufferedInput = new ArrayList<Byte>();
+		
+		while( is.read(buffer) >= 0 ){
+			
+			os.write(buffer);
+			os.write("Buffer\n".getBytes());
+			
+			for(int i = 0; i < buffer.length; i++){
+				
+				if(buffer[i] == (byte)'\n'){
+					
+					byte[] output = new byte[bufferedInput.size()];
+					int index = 0;
+					for(Byte b : bufferedInput){
+						output[index++] = (byte) b;
+					}
+					os.write("Buffered output: ".getBytes());
+					os.write(output);
+					os.flush();
+					bufferedInput.clear();
+				} else {
+					bufferedInput.add(buffer[i]);
+				}
+				
+			}
+			
+			buffer = new byte[buffer.length];
+		}
+		
+		
+	}
+
+}
---
+++ src/ba/bitcamp/mustafaademovic/Streams/UcitavanjeIspisivanjeStreama.java
@@ -0,0 +1,25 @@
+package ba.bitcamp.mustafaademovic.Streams;
+
+/**
+ * Ucitavanje i ispisivanje pomocu streama
+ */
+
+import java.io.*;
+
+public class UcitavanjeIspisivanjeStreama {
+
+	public static void main(String[] args) throws IOException {
+		System.out.println("Write some text: ");
+		InputStream is = new DataInputStream(System.in);
+		OutputStream os = new DataOutputStream(System.out);
+		byte[] buffer = new byte[20];
+
+		while (is.read(buffer) >= 0) {
+
+			os.write(buffer);
+			buffer = new byte[20];
+
+		}
+	}
+
+}
